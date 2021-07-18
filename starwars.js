0e940ce1a1d823949eb4d2030f7f26cad3851bb7
Vinícius França <viniciusfdev@gmai.com>
2021-07-18 11:16:46 -0300

    exercicio 1

--- starwars.js
+++ starwars.js
@@ -1,7 +1,10 @@
-// Seu javascript aqui :)
-// Use a Star Wars API: https://swapi.dev/
-// para fazer uma requisição assíncrona e:
-//  - Pegar a lista de filmes (AJAX) e preencher no HTML
-//  - Quando um filme for clicado, exibir sua introdução
+import { play } from "./music-sem-private";
 
-const API_ENDPOINT = 'https://swapi.dev/api'
+const API_ENDPOINT = "https://swapi.dev/api";
+
+play({
+  audioUrl: "audio/tema-sw.mp3",
+  title: "Star Wars Theme",
+  artist: "Unknow",
+  coverImageUrl: "imgs/logo.svg",
+});
