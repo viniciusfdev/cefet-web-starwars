873dc65e3d7d63a6fa82e0f60cc412e72e3b113b
Vinícius França <viniciusfdev@gmai.com>
2021-07-18 14:09:48 -0300

    exercicio 2

---
+++ elements.js
@@ -0,0 +1,1 @@
+export const filmList = document.querySelector("#filmes ul");
--- starwars.js
+++ starwars.js
@@ -1,11 +1,37 @@
-import { play } from "./music.js";
+import { filmList } from './elements.js';
+import { play } from './music.js';
+import { romanize } from './roman.js';
 
-const API_ENDPOINT = "https://swapi.dev/api";
+const API_ENDPOINT = 'https://swapi.dev/api';
 
-play({
-  audioUrl: "audio/tema-sw.mp3",
-  title: "Star Wars Theme",
-  artist: "Unknow",
-  coverImageUrl: "./imgs/logo.svg",
-}, document.body)
+play(
+  {
+    audioUrl: 'audio/tema-sw.mp3',
+    title: 'Star Wars Theme',
+    artist: 'Unknow',
+    coverImageUrl: './imgs/logo.svg',
+  },
+  document.body
+);
 
+function FilmItem(film) {
+  const el = document.createElement("li");
+  el.innerHTML = `Episode ${romanize(film.episode_id)} - ${film.title}`
+  return el;
+}
+
+async function fillFilmList() {
+  filmList.innerHTML = '';
+
+  const films = await fetch(`${API_ENDPOINT}/films`)
+    .then((response) => response.json())
+    .then(({ results }) => results);
+
+  if (Array.isArray(films)) {
+    films.forEach((film) => {
+      filmList.appendChild(FilmItem(film));
+    });
+  }
+}
+
+fillFilmList();
