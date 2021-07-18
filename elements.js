f140a7360bde1aabc722d5ca4f7ffc9e06b4a50c
Vinícius França <viniciusfdev@gmai.com>
2021-07-18 14:30:25 -0300

    exercicio 3 - exercicio 4

--- elements.js
+++ elements.js
@@ -1,1 +1,2 @@
-export const filmList = document.querySelector("#filmes ul");
+export const filmList = document.querySelector('#filmes ul');
+export const intro = document.querySelector('pre.introducao');
--- starwars.js
+++ starwars.js
@@ -1,8 +1,10 @@
-import { filmList } from './elements.js';
+import { filmList, intro } from './elements.js';
 import { play } from './music.js';
+import { restartAnimation } from './restart-animation.js';
 import { romanize } from './roman.js';
 
 const API_ENDPOINT = 'https://swapi.dev/api';
+let films = [];
 
 play(
   {
@@ -14,24 +16,43 @@ play(
   document.body
 );
 
-function FilmItem(film) {
-  const el = document.createElement("li");
-  el.innerHTML = `Episode ${romanize(film.episode_id)} - ${film.title}`
+function FilmItem(film, index) {
+  const el = document.createElement('li');
+  el.innerHTML = `Episode ${romanize(film.episode_id)} - ${film.title}`;
+  el.onclick = () => loadIntroduction(film);
   return el;
 }
 
 async function fillFilmList() {
   filmList.innerHTML = '';
 
-  const films = await fetch(`${API_ENDPOINT}/films`)
-    .then((response) => response.json())
-    .then(({ results }) => results);
-
   if (Array.isArray(films)) {
-    films.forEach((film) => {
-      filmList.appendChild(FilmItem(film));
-    });
+    films
+      .sort((a, b) => (a.episode_id < b.episode_id ? -1 : 1))
+      .forEach((film, index) => filmList.appendChild(FilmItem(film, index)));
   }
+  
+  restartAnimation(intro);
 }
 
-fillFilmList();
+async function loadFilms() {
+  films = (
+    await fetch(`${API_ENDPOINT}/films`)
+      .then((response) => response.json())
+      .then(({ results }) => results)
+  );
+
+  console.log(films); 
+}
+
+function loadIntroduction(film) {
+  intro.innerHTML =
+    `Episode ${film.episode_id}\r` +
+    `${film.title}\r` +
+    `\r${film.opening_crawl}\r`;
+  restartAnimation(intro);
+}
+
+loadFilms().then(() => {
+  fillFilmList();
+});
